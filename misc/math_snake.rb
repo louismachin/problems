4e2d5faa915d02412a034fb91981a94e04df7b70
louis <louis@machin.dev>
2026-01-04 00:01:39 +0000

    Added math snake question

--- misc/math_snake.rb
+++ misc/math_snake.rb
@@ -4,17 +4,20 @@
 # I wanted to try solving while watching the video,
 # it didn't seem like too many options to bruteforce!
 
-# My solution:
-# 1 + 13 * 2 / 3 + 4 + 12 * 5 - 7 - 11 + 8 * 6 / 9 - 10 = 66
+# Found 144 solutions
+# e.g. 8 + 13 * 2 / 1 + 6 + 12 * 3 - 5 - 11 + 7 * 4 / 9 - 10 = 66
 
-options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+solutions = [] 
 
-options.permutation.each do |a, b, c, d, e, f, g, h, i|
-    rhs = ((((((((((((a + 13) * b) / c) + d) + 12) * e) - f) - 11) + g) * h) / i) - 10)
-    if rhs == 66
-        puts "#{a} + 13 * #{b} / #{c} + #{d} + 12 * #{e} - #{f} - 11 + #{g} * #{h} / #{i} - 10 = 66"
-        exit
-    end
+[1, 2, 3, 4, 5, 6, 7, 8, 9].permutation.each do |a, b, c, d, e, f, g, h, i|
+    rhs = ((((((((((((a + 13) * b) / c.to_f) + d) + 12) * e) - f) - 11) + g) * h) / i.to_f) - 10)
+    solutions << [a, b, c, d, e, f, g, h, i] if rhs == 66
 end
 
-puts 'No solution'
+if solutions.any?
+    puts "Found #{solutions.count} solutions"
+    a, b, c, d, e, f, g, h, i = solutions.sample
+    puts "e.g. #{a} + 13 * #{b} / #{c} + #{d} + 12 * #{e} - #{f} - 11 + #{g} * #{h} / #{i} - 10 = 66"
+else
+    puts "No solutions"
+end
